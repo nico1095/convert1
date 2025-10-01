4c9cce4a5ce1ebda265f840bb92b727084705867
nico1095 <agiso@students.kennesaw.edu>
2025-10-01 02:20:51 -0400

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,6 +2,6 @@
 # Course: IT3883/Section W02
 # Student Name: Anthony Giso
 # Assignment Number: 3
-# Due Date: 09/19/ 2025
-# Purpose: python program that calculates the final averages for a group of students and print the results in descending order by grade. 
+# Due Date: 10/03/ 2025
+# Purpose: GUI application to convert Miles per Gallon into Kilometers per Liter.  
 # Used Python module and chapters.
